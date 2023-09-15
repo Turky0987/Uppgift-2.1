510f08b9e8e02b9cb6d76b5bb357f053f1c9ebd2
taal04003 <taal04003@E-PW0234KH>
2023-09-15 12:49:05 +0200

    Add project files.

---
+++ uppgift 2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_2._3 
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilken är ditt favo bok");
+            string favobok = Console.ReadLine();
+            Console.WriteLine("din favo bok är " + favobok);
+        }
+    
+
+
+     
+
+    }
+}
